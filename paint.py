66028aea385683d649358af3260c081786260871
bing1100 <bingxuhu@gmail.com>
2024-07-09 15:54:06 -0400

    working decorator

--- paint.py
+++ paint.py
@@ -1,6 +1,6 @@
 from tkinter import *
 from tkinter.colorchooser import askcolor
-from PIL import Image, ImageDraw
+from signature import Signature, SignatureConfigurator
 
 class Paint(object):
 
@@ -15,20 +15,16 @@ class Paint(object):
         self.pen_button.grid(row=0, column=i)
 
         i+=1
-        self.coord_button = Button(self.root, text='coord', command=self.use_coord)
+        self.coord_button = Button(self.root, text='dot-pen', command=self.use_coord)
         self.coord_button.grid(row=0, column=i)
 
-        i+=1
-        self.brush_button = Button(self.root, text='brush', command=self.use_brush)
-        self.brush_button.grid(row=0, column=i)
+        # i+=1
+        # self.brush_button = Button(self.root, text='brush', command=self.use_brush)
+        # self.brush_button.grid(row=0, column=i)
 
-        i+=1
-        self.color_button = Button(self.root, text='color', command=self.choose_color)
-        self.color_button.grid(row=0, column=i)
-
-        i+=1
-        self.eraser_button = Button(self.root, text='eraser', command=self.use_eraser)
-        self.eraser_button.grid(row=0, column=i)
+        # i+=1
+        # self.eraser_button = Button(self.root, text='eraser', command=self.use_eraser)
+        # self.eraser_button.grid(row=0, column=i)
 
         i+=1
         self.undo_button = Button(self.root, text='undo', command=self.use_undo)
@@ -37,20 +33,81 @@ class Paint(object):
         i+=1
         self.ce_button = Button(self.root, text="CE", command=self.use_ce)
         self.ce_button.grid(row=0, column=i)
-        
+
         i+=1
-        self.choose_size_button = Scale(self.root, from_=1, to=10, orient=HORIZONTAL)
+        self.color_button = Button(self.root, text='color', command=self.choose_color)
+        self.color_button.grid(row=0, column=i)
+
+        i+=1
+        self.choose_size_button = Scale(self.root, showvalue=0, label="Size", from_=1, to=10, orient=HORIZONTAL)
         self.choose_size_button.grid(row=0, column=i)
 
         i+=1
-        self.c = Canvas(self.root, bg='white', width=612, height=792)
-        self.c.grid(row=1, columnspan=i)
+        self.finish_button = Button(self.root, text="finish", command=self.finish)
+        self.finish_button.grid(row=0, column=i)
 
-        self.coords = [[]]
+        i = 0
+        self.yfade = BooleanVar(value=False)
+        self.yfade_button = Checkbutton(self.root, text="Y-Fade", variable=self.yfade, onvalue=True, offvalue=False)
+        self.yfade_button.grid(row=1, column=i)
+
+        i+=1
+        self.ydrift = BooleanVar(value=False)
+        self.ydrift_button = Checkbutton(self.root, text="Y-Drift", variable=self.ydrift, onvalue=True, offvalue=False)
+        self.ydrift_button.grid(row=1, column=i)
+
+        i+=1
+        self.xstretch = BooleanVar(value=False)
+        self.xstretch_button = Checkbutton(self.root, text="X-Stretch", variable=self.xstretch, onvalue=True, offvalue=False)
+        self.xstretch_button.grid(row=1, column=i)
+
+        i+=1
+        self.jiggle = BooleanVar(value=False)
+        self.jiggle_button = Checkbutton(self.root, text="Jiggle", variable=self.jiggle, onvalue=True, offvalue=False)
+        self.jiggle_button.grid(row=1, column=i)
+
+        i+=1
+        self.velocity = BooleanVar(value=False)
+        self.velocity_button = Checkbutton(self.root, text="Velocity", variable=self.velocity, onvalue=True, offvalue=False)
+        self.velocity_button.grid(row=1, column=i)
+
+        i+=1
+        self.dotshift = BooleanVar(value=False)
+        self.dotshift_button = Checkbutton(self.root, text="Dot-Shift", variable=self.dotshift, onvalue=True, offvalue=False)
+        self.dotshift_button.grid(row=1, column=i)
+
+        i = 0
+        self.stampbox = BooleanVar(value=False)
+        self.stampbox_button = Checkbutton(self.root, text="Stamp-Box", variable=self.stampbox, onvalue=True, offvalue=False)
+        self.stampbox_button.grid(row=2, column=i)
+
+        # i+=1
+        # self.circle = 0
+        # self.circle_button = Checkbutton(self.root, text="Stamp-Circle", variable=self.circle, onvalue=True, offvalue=False)
+        # self.circle_button.grid(row=2, column=i)
+
+        i+=1
+        self.rotational = BooleanVar(value=False)
+        self.rotational_button = Checkbutton(self.root, text="Rotational", variable=self.rotational, onvalue=True, offvalue=False)
+        self.rotational_button.grid(row=2, column=i)
+
+        i+=1
+        self.choose_ink_button = Scale(self.root, showvalue=0, label="Ink", from_=1, to=10, orient=HORIZONTAL)
+        self.choose_ink_button.grid(row=2, column=i)
+
+        i+=1
+        self.label = Label(self.root, text="Seed:")
+        self.label.grid(row=2, column=i)
+        i+=1
+        self.seed_entry= Entry(self.root, width= 15)
+        self.seed_entry.grid(row=2, column=i)
+
+        i+=1
+        self.c = Canvas(self.root, bg='white', width=900, height=900)
+        self.c.grid(row=3, columnspan=8)
+
+        self.coords = []
         self.splines = []
-
-        self.img = Image.new('RGB', (612, 792), (255,255,255))
-        self.draw = ImageDraw.Draw(self.img)
 
         self.setup()
         self.root.mainloop()
@@ -59,6 +116,12 @@ class Paint(object):
         self.old_x = None
         self.old_y = None
         self.line_width = self.choose_size_button.get()
+        self.fade = self.choose_ink_button.get()
+        self.seed = self.seed_entry.get()
+        try:
+            self.seed = int(self.seed)
+        except:
+            self.seed = 0
         self.color = self.DEFAULT_COLOR
         self.eraser_on = False
         self.active_button = self.pen_button
@@ -82,37 +145,38 @@ class Paint(object):
             
 
     def use_pen(self):
+        self.reset()
         self.c.bind('<B1-Motion>', self.paint)
         self.c.bind('<ButtonRelease-1>', self.reset)
         self.c.unbind('<ButtonPress-1>')
         self.activate_button(self.pen_button)
 
     def use_coord(self):
+        self.reset()
         self.c.unbind('<B1-Motion>')
         self.c.unbind('<ButtonRelease-1>')
         self.c.bind('<ButtonPress-1>', self.paint)
         self.activate_button(self.coord_button)
 
-    def use_brush(self):
-        self.c.bind('<B1-Motion>', self.paint)
-        self.c.bind('<ButtonRelease-1>', self.reset)
-        self.c.unbind('<ButtonPress-1>')
-        self.activate_button(self.brush_button)
+    # def use_brush(self):
+    #     self.c.bind('<B1-Motion>', self.paint)
+    #     self.c.bind('<ButtonRelease-1>', self.reset)
+    #     self.c.unbind('<ButtonPress-1>')
+    #     self.activate_button(self.brush_button)
 
     def choose_color(self):
         self.eraser_on = False
         self.color = askcolor(color=self.color)[1]
 
-    def use_eraser(self):
-        self.c.bind('<B1-Motion>', self.paint)
-        self.c.bind('<ButtonRelease-1>', self.reset)
-        self.c.unbind('<ButtonPress-1>')
-        self.activate_button(self.eraser_button, eraser_mode=True)
+    # def use_eraser(self):
+    #     self.c.bind('<B1-Motion>', self.paint)
+    #     self.c.bind('<ButtonRelease-1>', self.reset)
+    #     self.c.unbind('<ButtonPress-1>')
+    #     self.activate_button(self.eraser_button, eraser_mode=True)
 
     def use_ce(self):
         self.c.delete("all")
-        self.coords = [[]]
-        self.splines = []
+        self.coords = []
 
     def activate_button(self, some_button, eraser_mode=False):
         self.active_button.config(relief=RAISED)
@@ -132,9 +196,35 @@ class Paint(object):
                                capstyle=ROUND, smooth=TRUE, splinesteps=36)
         self.old_x = x
         self.old_y = y
+        if len(self.coords) == 0:
+            self.coords.append([])
+
         if self.eraser_on == False and redraw == False:
             self.coords[-1].append((x, y))
+        
+    def finish(self):
+        # sig = Signature(self.coords, self.color, self.line_width, 1)
+        # sig.write_img("test_sig")
+        # sig.write_ps("test_sig")
 
+        sig_config = SignatureConfigurator()
+        sig_config.generate_sigs(
+            self.coords,
+            self.color,
+            self.line_width,
+            self.fade,
+
+            seed = self.seed,
+
+            yfade = self.yfade.get(),
+            ydrift = self.ydrift.get(),
+            xstretch = self.xstretch.get(),
+            jiggle = self.jiggle.get(),
+            velocity = self.velocity.get(),
+            dotshift = self.dotshift.get(),
+            stampbox = self.stampbox.get(),
+            rotational = self.rotational.get(),
+        )
 
     def reset(self, event=None):
         self.old_x, self.old_y = None, None
--- signature.py
+++ signature.py
@@ -2,25 +2,13 @@ from tkinter import *
 from PIL import Image, ImageDraw
 import numpy as np
 import scipy.interpolate as si
+import scipy.integrate as it
 
 import sys
 from tkinter import Tk, Button, Frame
 from tkinter.scrolledtext import ScrolledText
 
-def write_ps(fname, splines):
-    line = ""
-    for spline in splines:
-        line += "newpath\n"
-        line += "{} {} moveto\n".format(spline[0][0], 792-spline[0][1])
-        for v in spline[1:]:
-            line += "{} {} lineto\n".format(v[0], 792-v[1])
-        line += "2 setlinewidth\n"
-        line += "stroke\n"
-    line += "showpage\n"
-
-    with open("./"+fname, 'w') as f:
-        f.writelines(line)
-    
+from PIL import Image, ImageDraw
 
 def bspline(cv, n=100, degree=3):
     """ Calculate n samples on a bspline
@@ -43,4 +31,118 @@ def bspline(cv, n=100, degree=3):
 
     # Calculate result
     return np.array(si.splev(u, (kv,cv.T,degree))).T
+
+class SignatureComponent():
+    def update_sig(self, seed):
+        pass
+
+class Signature(SignatureComponent):
+    def __init__(self, coords, color, width, fade) -> None:
+        if color == "black":
+            color = "#000000"
+        color = color.lstrip('#')
+        color = tuple(int(color[i:i+2], 16) for i in (0, 2, 4))
+
+        self.splines = []
+        for line in coords:
+            if len(line) == 0:
+                continue
+            spline = bspline(line, n=int(len(line)//3))
+            self.splines.append(spline)
+
+        a = 0
+        self.lengths = [0]+[a:=a+len(spline) for spline in self.splines]
+        self.splines = np.concatenate(self.splines)
+        self.color = np.zeros((len(self.splines), 3)) + color
+        self.fade = np.ones(len(self.splines)) * ((fade-1)/10)
+        self.width = np.ones(len(self.splines)) * width
+
+        print(self.lengths)
+
+    def update_sig(self, seed):
+        pass
+
+    def get_sig(self):
+        splines = [self.splines[a:b] for a,b in zip(self.lengths[:-1], self.lengths[1:])]
+        colors = [self.color[a:b] for a,b in zip(self.lengths[:-1], self.lengths[1:])]
+        fades = [self.fade[a:b] for a,b in zip(self.lengths[:-1], self.lengths[1:])]
+        widths = [self.width[a:b] for a,b in zip(self.lengths[:-1], self.lengths[1:])]
+        return splines, colors, fades, widths
+    
+    def write_img(self, fname):
+        img = Image.new('RGB',(900,900),(255,255,255))
+        draw = ImageDraw.Draw(img)
+        splines, _, _, _ = self.get_sig()
+        for spline in splines:
+            if len(spline) == 0:
+                continue
+            draw.line([(x,y) for x,y in spline], fill='red', width=2)
+        img.save(f'./{fname}.png')
+
+    def write_ps(self, fname):
+        line = ""
+        splines, colors, fades, widths = self.get_sig()
+        for spline, color, fade, width in zip(splines, colors, fades, widths):
+            for v1, v2, c, f, w in zip(spline[:-1], spline[1:], color[1:], fade[1:], width[1:]):
+                line += "newpath\n"
+                line += f"{v1[0]} {900 - v1[1]} moveto\n"
+                line += f"{v2[0]} {900 - v2[1]} lineto\n"
+                line += f"1 setlinecap\n"
+                line += f"{w} setlinewidth\n"
+                line += f"{c[0]} {c[1]} {c[2]} setrgbcolor\n"
+                line += f"{f} setgray\n"
+                line += f"stroke\n"
+        line += "showpage\n"
+        with open(f'./{fname}.ps', 'w') as f:
+            f.writelines(line)
+
+class SignatureDecorator(SignatureComponent):
+    _sig: SignatureComponent = None
+
+    def __init__(self, signature, seed=0):
+        self._sig = signature
+        self.seed = seed
+        self.update_sig()
+    
+    @property
+    def signature(self) -> SignatureComponent:
+        return self._sig
+
+    def update_sig(self):
+        return self._sig.update_sig(self.seed)
+
+class SigJiggle(SignatureDecorator):
+    def update_sig(self):
+        rng = np.random.default_rng(self.seed + 1234)
+        a = rng.normal(0, 0.1, (len(self.signature.splines),2))
+        v = np.stack([
+            it.cumulative_trapezoid(a[:,0], initial=0), 
+            it.cumulative_trapezoid(a[:,0], initial=0)
+        ], axis=-1)
+        l = np.stack([
+            it.cumulative_trapezoid(v[:,0], initial=0), 
+            it.cumulative_trapezoid(v[:,0], initial=0)
+        ], axis=-1)
+        print(l)
+        print(l.shape)
+        print(self.signature.splines.shape)
+        self.signature.splines += l
+
+class SignatureConfigurator():
+    def generate_sigs(self, coords, color, width, fade, **kwargs):
+        sig = Signature(coords, color, width, fade)
+        seed = kwargs.get('seed', 0)
+
+        if kwargs['jiggle']:
+            sig = SigJiggle(sig, seed)
+
+        sig.signature.write_img('test_sig')
+        sig.signature.write_ps('test_sig')
+
+
+
+
+    
+
+
 
